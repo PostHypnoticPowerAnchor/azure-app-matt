ddea95ae790f5e146c1253d0c94fed071682e7b1
PostHypnoticPowerAnchor <matthias.salow@iamds.com>
2024-11-16 13:28:36 +0100

    AppInsights rausgeschmissen

--- obj/Debug/net9.0/azure-app-matt.AssemblyInfo.cs
+++ obj/Debug/net9.0/azure-app-matt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure-app-matt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5def36a7063743adc6c1f9d251322f219f7aa747")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ca08a66dcc7b4fc3d683c6f8b470a1e30c22c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure-app-matt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure-app-matt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
